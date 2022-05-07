38bbce0c0de4b3e788ff08cdbcfe9ec20616da14
Lionel Regisseur <regisseur.lionel@gmail.com>
2022-05-07 21:35:03 +0200

    Script classDiceFace renommé en throwTheDice

--- script/throwTheDice.js
+++ script/throwTheDice.js
@@ -4,9 +4,19 @@ function throwTheDice() {
     
     useRollDice.addEventListener("click", e => {
         randomDice(6);
+        changeFaceDice();
     });
     useDice.addEventListener("click", e => {
         randomDice(6);
+        changeFaceDice();
     });
 };
 throwTheDice();
+
+// TODO création d'une fonction faceDice qui affiche le dé correspondant au resultat de randomDice()
+
+function changeFaceDice() {
+    let displayFaceDice = document.querySelector('feature__dice-img');
+    // displayFaceDice.src.match = './images/dice-2.svg';
+    displayFaceDice.src
+};
