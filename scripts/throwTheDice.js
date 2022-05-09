150bbb3c529684e47ba32025a683b517a695db9e
Lionel Regisseur <regisseur.lionel@gmail.com>
2022-05-09 14:03:04 +0200

    Fusion du script throwtheDice dans dice.js

--- scripts/throwTheDice.js
+++ scripts/throwTheDice.js
@@ -1,7 +1,4 @@
 function throwTheDice() {
-    const useRollDice = document.querySelector('.feature__rollDice');
-    const useDice = document.querySelector('.feature__dice');
-    
     useRollDice.addEventListener("click", e => {
         randomDice(6);
         changeFaceDice();
@@ -13,12 +10,13 @@ function throwTheDice() {
 };
 throwTheDice();
 
-// TODO création d'une fonction faceDice qui affiche le dé correspondant au resultat de randomDice()
+// TODO: création d'une fonction faceDice qui affiche le dé correspondant au resultat de randomDice()
 
 function changeFaceDice() {
     let displayFaceDice = document.querySelector('feature__dice-img');
     // displayFaceDice.src.match = './images/dice-2.svg';
     displayFaceDice.src
+    
 };
 
 // Si scoreDice = 2 alors faceDice 
